12979df17f6f3fe823e6dbed72866c99211e47a5
Tovly Deutsch <tovlydeutsch@gmail.com>
2016-12-04 16:43:19 -0500

    added basic file view

--- fileList.js
+++ fileList.js
@@ -1,12 +1,13 @@
 // This function list the files in tab 2 for sorting and grouping
 function listFiles(files) {
-  var fileTable = '<table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">\
+  console.log(files[0])
+  var fileTable = '<table id="file-list" class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">\
   <thead>\
     <tr>\
       <th class="mdl-data-table__cell--non-numeric">File Name</th>\
-      <th>Quota Used</th>\
-      <th>Date Created</th>\
-      <th>Owner</th>\
+      <th class="narrow">Quota Used</th>\
+      <th class="narrow">Date Created</th>\
+      <th class="narrow">Owner</th>\
     </tr>\
   </thead>\
   <tbody>'
@@ -16,12 +17,17 @@ function listFiles(files) {
     fileTable += '\
     <tr>\
       <td class="mdl-data-table__cell--non-numeric">' + files[i].name + '</td>\
-      <td>' + files[i].quotaBytesUsed + '</td>\
-      <td>' + files[i].createdTime + '</td>\
-      <td></td>\
+      <td class="narrow">' + files[i].quotaBytesUsed + '</td>\
+      <td class="narrow">' + files[i].createdTime + '</td>\
+      <td class="narrow"><img class="small-avatar"src="' + files[i].owners[0].photoLink + '"/>' + files[i].owners[0].displayName + '</td>\
     </tr>'
   }
 
+
   fileTable += '</tbody></table>'
   document.getElementById('content2').innerHTML = fileTable
+
+  $( "#file-list tbody" ).on( "click", "tr", function() {
+    $( this ).toggleClass('selected');
+  });
 }
