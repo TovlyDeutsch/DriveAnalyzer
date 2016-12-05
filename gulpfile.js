a11e2edf04d400183c41bf440d7d40170ca25eec
Tovly Deutsch <tovlydeutsch@gmail.com>
2016-12-04 22:58:05 -0500

    modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
 var gulp = require('gulp')
 var uglify = require('gulp-uglify')
+var removeCode = require('gulp-remove-code');
 
-gulp.task('default', function() {
+gulp.task('js', function() {
   gulp.src('js/*.js')
-  .pipe(uglify())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('css', function() {
+  gulp.src('style.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist'));
+})
+
+gulp.task('html', function() {
+gulp.src('index.html')
+  .pipe(removeCode({ production: true }))
   .pipe(gulp.dest('dist'))
-});
+})
+
+gulp.task('default', ['js', 'css', 'html'])
