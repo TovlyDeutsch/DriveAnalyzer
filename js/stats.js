699229f2b166e4dc1917d45704e8046f4918bf4c
Darartu Gamada <Gamada@Darartus-MacBook-Pro.local>
2016-12-07 15:03:48 -0500

    attempt at adding button

--- js/stats.js
+++ js/stats.js
@@ -34,25 +34,37 @@ function getFilesNotOwned(files) {
   return [filesOwned, filesNotOwned]
 }
 
+// creates piechart
 function displayGraph(files) {
 
+  // initialize to seperate arrays for the two different pie chart views
   var numberArray = [['Type', 'Number']]
   var quotaArray = [['Type', 'Quota Used']]
 
+  // track which file type is currently being iterated through
   var increment = 1
+
+  // create array of files seperated based on type
   var seperatedArray = seperateBy(files, 'type')
+  // increment through array based on type
   for (var type in seperatedArray) {
+    // add number of files to numberArray
     numberArray.push([type, seperatedArray[type].files.length])
+    // initialize quota used by type to 0
     quotaArray.push([type, 0])
     
+    // look through files in each file type
     for (var i in seperatedArray[type].files) {
+      // if file uses some quota
       if (seperatedArray[type].files[i].quotaBytesUsed != 0) {
+        // increment total quota used by file type
         quotaArray[increment][1] += parseInt(seperatedArray[type].files[i].quotaBytesUsed) 
       }
     }
     increment += 1
   }
 
+  // initialize variable to track which data to use
   var current = numberArray
 
   // the remaining lines below are directly taken and adapted from https://developers.google.com/chart/interactive/docs/gallery/piechart
@@ -60,17 +72,19 @@ function displayGraph(files) {
   google.charts.setOnLoadCallback(drawChart);
 
   function drawChart() {
-
+    // stores current chosen data to be displayed
     var data = google.visualization.arrayToDataTable(
        current
       );
+    // set chart title
     var options = {
       title: 'Distribution of file types'
     };
 
+    // 
     var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-    document.getElementById('piechart-button').innerHTML = "hgfghfghfghjfghfghjfghfghfghfghf";
 
+    // draws chart
     chart.draw(data, options);
     $('#graph-loader').remove()
   }
